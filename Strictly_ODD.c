10beefe6f3c447d279d561ae8842165a71e99eeb
dharanitirumalasetti <147476313+dharanitirumalasetti@users.noreply.github.com>
2024-01-18 21:06:13 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int flag=1;     for(i=0;i<n;i++)     {         if(arr[i]%2!=0&&i%2==0)         flag=0;     }     if(flag)     printf("True");     else     printf("False"); }

---
+++ Strictly_ODD.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int flag=1;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0&&i%2==0)
+        flag=0;
+    }
+    if(flag)
+    printf("True");
+    else
+    printf("False");
+}
