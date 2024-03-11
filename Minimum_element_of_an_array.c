1f2cbe0f993220d846475bd935971e03c3ba7523
dharanitirumalasetti <147476313+dharanitirumalasetti@users.noreply.github.com>
2024-03-11 21:54:44 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[0]>arr[i])         arr[0]=arr[i];     }     printf("%d",arr[0]); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[0]>arr[i])
+        arr[0]=arr[i];
+    }
+    printf("%d",arr[0]);
+}
